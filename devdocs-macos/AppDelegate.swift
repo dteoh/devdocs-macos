f36e26ecb81475026d9d17957d97f984a39cfe40
Douglas Teoh <douglas@dteoh.com>
2018-12-26 15:39:48 +1000

    Add Option+space global hotkey

--- devdocs-macos/AppDelegate.swift
+++ devdocs-macos/AppDelegate.swift
@@ -1,17 +1,30 @@
 import Cocoa
+import HotKey
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    private var hotKey: HotKey!
 
     func applicationWillFinishLaunching(_ notification: Notification) {
         let _ = DocumentationController.shared
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        setupHotKey()
         DocumentationController.shared.newDocument(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    private func setupHotKey() {
+        hotKey = HotKey(key: .space, modifiers: [.option])
+        hotKey.keyDownHandler = hotKeyPressed
+    }
+
+    private func hotKeyPressed() {
+        NSApp.activate(ignoringOtherApps: true)
     }
 }
